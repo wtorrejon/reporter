bbe12bb4ec9c6338039ce01794705c205173c990
William Torrejon Celis <w.torrejon10@gmail.com>
2024-12-12 10:08:38 -0500

    add react inyections components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 
-interface HeaderProps {
-    title: string;
-}
+const Header = () => {
+  return (
+    <header>
+      <nav className=" bg-[#005b1c] rounded-[30px] navbar flex items-center justify-between h-[60px] m-6">
+        <div className="logo p-6">
+          <a className=" font-[poppins] font-medium text-[24px] text-white" href="/">
+            Cabieses
+          </a>
+        </div>
+        <div className=" flex gap-4">
+          <div className=" relative">
+            <span className=" text-white material-symbols-outlined">
+              notifications
+            </span>
+            <span className=" absolute top-[-5px] right-[-5px] bg-red-900 w-4 h-4 rounded-[30px] flex items-center justify-center text-white text-[10px]">
+              1
+            </span>
+            <div></div>
+          </div>
+          <div className=" pr-6">
+            <span className=" text-white material-symbols-outlined">
+              account_circle
+            </span>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-///explicame que es esto
-const Header: React.FC<HeaderProps> = ({ title }) => {
-    return (
-        <header className="container mx-auto ">
-                <h1>CABIESES APP</h1>
-        </header>
-    );
-};  
 export default Header;
